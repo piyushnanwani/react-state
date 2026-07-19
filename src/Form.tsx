5d53dad918360f1d0f4e581a46f5284afae6e595
Piyush Nanwani <pnanwani61@gmail.com>
2026-07-19 11:49:46 +0530

    keyboard events

--- src/Form.tsx
+++ src/Form.tsx
@@ -11,12 +11,23 @@ const FormComponent: React.FC = () => {
   function handleChange (event: React.ChangeEvent<HTMLFormElement>) {
     setName(event.target.value);
   }
+
+  const handleKeyDown = (event: React.KeyboardEvent<HTMLFormElement>) => {
+    console.log("Key ", event.key)
+  }
+
+  const handleKeyUp = (event: React.KeyboardEvent<HTMLFormElement>) => {
+      console.log("Key ", event.key);
+  };
+
   return (
     <form onSubmit={handleSubmit} >
       <input 
         value={name}
         type="text"
         onChange={handleChange}
+        onKeyUp={handleKeyUp}
+        onKeyDown={handleKeyDown}
         placeholder= "Enter your name"
        />
        <button type="submit">Submit</button>
